67b4e61ba2f00bc669a07ecfb76bbcd10c53bd3d
andreavalle0888 <42286620+andreavalle0888@users.noreply.github.com>
2018-10-19 15:08:13 -0500

    Add files via upload

---
+++ 1deoctubre2018/1deoctubre2018/Iniciosesion.swift
@@ -0,0 +1,25 @@
+//
+//  Iniciosesion.swift
+//  1deoctubre2018
+//
+//  Created by macbook on 19/10/18.
+//  Copyright © 2018 macbook. All rights reserved.
+//
+
+import UIKit
+
+class Iniciosesion: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+
+}
--- 1deoctubre2018/1deoctubre2018/ViewController.swift
+++ 1deoctubre2018/1deoctubre2018/ViewController.swift
@@ -30,25 +30,39 @@ class ViewController: UIViewController {
         let ingresodato = login(nombreregistardo: caja.text!, contraseñaresgitrada: ingrecontra.text!)
         ingresa.append(ingresodato)
         // para acceder con  los datos
+       
         var size1 = registracion.count
         print(size1)
         var i = 0
+        if (ingresa.count != 0) && (registracion.count == 0){
+            
+            let myAlert2 = UIAlertController(title: "Mensaje", message: "Debes registrarte.Es GRATIS", preferredStyle: .alert)
+            myAlert2.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(myAlert2, animated: true, completion: nil)
+            
+        }else{
+    
         while i < size1 {
             if(ingresa[i].nombreregistardo == registracion[i].nombre) && (ingresa[i].contraseñaresgitrada == registracion[i].contraseña){
             performSegue(withIdentifier: "ingresoacces", sender: nil)
-          }
+            }else{
+                let myAlert = UIAlertController(title: "No Se Encuentra El Usuario", message: "Haz Una Cuenta ¡¡Es Gratis!!", preferredStyle: .alert)
+                myAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(myAlert, animated: true, completion: nil)
+            }
           size1 -= 1
         }
-       
-    }
+     
+        }}
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+   
     
     
       
+    }
 
-}
 
